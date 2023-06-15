528d18e23d0ff61f5706f7771348122517c97c5c
Hafedhamza <hafedhamza63@gmail.com>
2023-06-15 12:32:16 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** the function main*/
+/**
+ *the function main
+ */
 int main(void)
-/** print a text */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/** the function main must return int*/
 	return (0);
 }
