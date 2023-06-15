a0e0a51a33dd821058467169c571fb8870a1731a
Hafedhamza <hafedhamza63@gmail.com>
2023-06-15 16:49:19 +0100

    c file

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 2, b = 3, c = 4;
+int sum = 0;
+sum = a + b + c;
+printf("the result is %d", sum);
+return (0);
+}
